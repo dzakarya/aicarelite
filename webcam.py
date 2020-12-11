a1a03714fd5bae6774d47257fdef981cc463b5d0
dzakarya <muhammadzakaria181094@gmail.com>
2020-12-11 12:18:34 +0700

    added keras

--- webcam.py
+++ webcam.py
@@ -9,50 +9,7 @@ from threading import Thread
 import importlib.util
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.resnet50 import preprocess_input
-# Define VideoStream class to handle streaming of video from webcam in separate processing thread
-# Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
-class VideoStream:
-    """Camera object that controls video streaming from the Picamera"""
-
-    def __init__(self, resolution=(1920, 1080), framerate=30):
-        # Initialize the PiCamera and the camera image stream
-        self.stream = cv2.VideoCapture(0)
-        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-        ret = self.stream.set(3, resolution[0])
-        ret = self.stream.set(4, resolution[1])
-
-        # Read first frame from the stream
-        (self.grabbed, self.frame) = self.stream.read()
-
-        # Variable to control when the camera is stopped
-        self.stopped = False
-
-    def start(self):
-        # Start the thread that reads frames from the video stream
-        Thread(target=self.update, args=()).start()
-        return self
-
-    def update(self):
-        # Keep looping indefinitely until the thread is stopped
-        while True:
-            # If the camera is stopped, stop the thread
-            if self.stopped:
-                # Close camera resources
-                self.stream.release()
-                return
-
-            # Otherwise, grab the next frame from the stream
-            (self.grabbed, self.frame) = self.stream.read()
-
-    def read(self):
-        # Return the most recent frame
-        return self.frame
-
-    def stop(self):
-        # Indicate that the camera and thread should be stopped
-        self.stopped = True
-
-
+from imutils.video import VideoStream
 # Define and parse input arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
